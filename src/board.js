809dcdda0137a4d3332e68d430c35cc320c16224
Samet UCA <sametuca@hotmail.com>
2023-04-27 21:35:32 +0300

    add square component

---
+++ src/board.js
@@ -0,0 +1,22 @@
+import Square from './square';
+export default function Board() {
+    return (
+        <>
+            <div className="board-row">
+                <Square />
+                <Square />
+                <Square />
+            </div>
+            <div className="board-row">
+                <Square />
+                <Square />
+                <Square />
+            </div>
+            <div className="board-row">
+                <Square />
+                <Square />
+                <Square />
+            </div>
+        </>
+    );
+}
